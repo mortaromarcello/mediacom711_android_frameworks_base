fa25c44ea9369301dde2fda3539743d38d7f8d27
mortaromarcello <mortaromarcello@gmail.com>
2012-12-17 12:15:14 +0100

    modified:   core/java/android/hardware/SensorEvent.java 	modified:   core/java/android/hardware/SystemSensorManager.java

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -238,6 +238,9 @@ public class SystemSensorManager extends SensorManager {
 			String  str = Settings.System.getString(mContext.getContentResolver(),
 					Settings.System.ACCELEROMETER_COORDINATE);
             int stype = sensor.getType();
+            t.originalValue[0]	= values[0];
+            t.originalValue[1]	= values[1];
+            t.originalValue[2]	= values[2];
             if(str!=null && str.equals("special")&&((stype == sensor.TYPE_ACCELEROMETER)||(stype == sensor.TYPE_GRAVITY))) {
             	    v[0] = values[1];
             	    v[1] = -values[0];
